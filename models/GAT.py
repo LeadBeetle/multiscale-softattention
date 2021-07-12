de178211a56b884b2e6ab7d79df70d2732e2bb24
LeadBeetle <dorian.mueller@outlook.de>
2021-07-12 19:25:55 +0200

    Update Notebooks and GAT

--- models/GAT.py
+++ models/GAT.py
@@ -1,13 +1,17 @@
 import torch
+
 from torch_geometric.nn import GATConv
 from torch.nn import Linear as Lin
+import torch.nn.functional as F
+from tqdm import tqdm
 
 
 class GAT(torch.nn.Module):
     def __init__(self, in_channels, hidden_channels, out_channels, num_layers,
-                 heads, dataset):
+                 heads, dataset, device):
         super(GAT, self).__init__()
 
+        self.device = device
         self.num_layers = num_layers
 
         self.convs = torch.nn.ModuleList()
@@ -49,7 +53,7 @@ class GAT(torch.nn.Module):
                 x = F.dropout(x, p=0.5, training=self.training)
         return x.log_softmax(dim=-1)
 
-    def inference(self, x_all):
+    def inference(self, x_all, loader):
         pbar = tqdm(total=x_all.size(0) * self.num_layers)
         pbar.set_description('Evaluating')
 
@@ -59,10 +63,10 @@ class GAT(torch.nn.Module):
         total_edges = 0
         for i in range(self.num_layers):
             xs = []
-            for batch_size, n_id, adj in subgraph_loader:
-                edge_index, _, size = adj.to(device)
+            for batch_size, n_id, adj in loader:
+                edge_index, _, size = adj.to(self.device)
                 total_edges += edge_index.size(1)
-                x = x_all[n_id].to(device)
+                x = x_all[n_id].to(self.device)
                 x_target = x[:size[1]]
                 x = self.convs[i]((x, x_target), edge_index)
                 x = x + self.skips[i](x_target)
