5365459db1c13a50fd48bc82a273c9c13643f717
LeadBeetle <dorian.mueller@outlook.de>
2021-08-16 15:46:24 +0200

    Add updated script

--- script.py
+++ script.py
@@ -1,32 +1,34 @@
 # To add a new cell, type '# %%'
 # To add a new markdown cell, type '# %% [markdown]'
 # %%
-from utils.experimentor import *
+from utils.experimentor_base import *
+from utils.experimentor_proteins import * 
 from utils.constants import *
 
 
 # %%
 config = {
-    "dataset_name": Dataset.OGBN_PRODUCTS,
+    "dataset_name": Dataset.OGBN_PROTEINS,
     "model_type": ModelType.GATV1,
-    'num_of_epochs': 10000,
+    'num_of_epochs': 1000,
     'num_of_runs': 3,
-    'patience_period': 200,
+    'patience_period': 50,
     
-    'batch_size': 256,
-    'test_batch_size': 256,
+    'batch_size': 1024,
+    'test_batch_size': 1024,
     'num_workers': 2,
     'force_cpu': False,
     'test_frequency': 5,
     'console_log_freq': 1,
     'do_train_tqdm_logging': False,
     
-    'lr': 0.001,
-    'num_of_layers': 3, 
+    'lr': 0.01,
+    'num_of_layers': 6, 
     'num_heads': 1,
-    'hidden_size': 128,
-    'dropout': 0.5,  
+    'hidden_size': 64,
+    'dropout': 0.25,  
     "use_layer_norm": False,
+    "use_batch_norm": True,
     
     'nbor_degree': 1,
     'adj_mode': AdjacencyMode.OneStep,
@@ -35,7 +37,8 @@ config = {
 
 
 # %%
-experimentor = Experimentor(config, logging)
+exp = Experimentor_Proteins if config["dataset_name"] == Dataset.OGBN_PROTEINS else Experimentor
+experimentor = exp(config)
 experimentor.run()
 
 
