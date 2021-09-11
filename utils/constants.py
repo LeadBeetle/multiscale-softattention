1828d7b47a1a3524836c6d2528d2922f245dda45
domu5238 <dorian.mueller@tu-ilmenau.de>
2021-09-11 11:56:57 +0200

    Update

--- utils/constants.py
+++ utils/constants.py
@@ -14,12 +14,19 @@ class Dataset(str, enum.Enum):
     OGBN_ARXIV = 'ogbn-arxiv',
     OGBN_PRODUCTS = 'ogbn-products',
     OGBN_PROTEINS = 'ogbn-proteins',
-    OGBN_MAG = 'ogbn-mag'
+    OGBN_MAG = 'ogbn-mag',
+    CORA = 'Cora',
+    CITESEER = 'CiteSeer',
+    PUBMED = 'PubMed'
     
 datasetMapping = {
     Dataset.OGBN_ARXIV: "ogbn-arxiv",
     Dataset.OGBN_PRODUCTS: "ogbn-products",
     Dataset.OGBN_PROTEINS: "ogbn-proteins",
-    Dataset.OGBN_MAG: "ogbn-mag"
+    Dataset.OGBN_MAG: "ogbn-mag",
+    Dataset.CORA: "Cora",
+    Dataset.CITESEER: "CiteSeer",
+    Dataset.PUBMED: "PubMed"
+
 }
     
---
+++ utils/experimentorUtils.py
@@ -0,0 +1,19 @@
+from utils.experimentor_base import *
+from utils.experimentor_proteins import * 
+from utils.experimentor_planetoid import * 
+from utils.constants import *
+
+def getExperimentor(dataset_name): 
+    isProteins = dataset_name == Dataset.OGBN_PROTEINS
+    isPlanetoid = dataset_name in [Dataset.CORA, Dataset.PUBMED, Dataset.CITESEER]
+    
+    exp = None
+    if isProteins:
+        exp = Experimentor_Proteins
+    elif isPlanetoid:
+        exp = Experimentor_Planetoid
+    else: 
+        exp = Experimentor
+       
+    assert(exp is not None)
+    return exp
